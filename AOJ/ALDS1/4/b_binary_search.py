bb8a489fe38fbfd156c756181fc71b543e81378a
Sora Tagami <loslonbervene.1923@icloud.com>
2020-08-11 23:20:21 +0900

    add ALDS1-4-B

---
+++ AOJ/ALDS1/4/b_binary_search.py
@@ -0,0 +1,29 @@
+def binary_search(n, S, T):
+    count = 0
+    for t in T:
+        left = 0
+        right = n
+        while left <= right:
+            middle = (left + right)//2
+            center = S[middle]
+            if t == center:
+                count += 1
+                break
+            elif t < center:
+                right = middle - 1
+            else:
+                left = middle + 1
+    return count
+
+
+def main():
+    n = int(input())
+    S = list(map(int, input().split()))
+    q = int(input())
+    T = list(map(int, input().split()))
+
+    print(binary_search(n, S, T))
+
+
+if __name__ == "__main__":
+    main()
