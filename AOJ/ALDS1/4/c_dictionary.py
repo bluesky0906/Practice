cdb82e404f09e56d498e911d30a88e8df9090f69
Sora Tagami <loslonbervene.1923@icloud.com>
2020-08-17 23:20:05 +0900

    add ALDS1-4C

---
+++ AOJ/ALDS1/4/c_dictionary.py
@@ -0,0 +1,51 @@
+# オープンハッシュ法を使うとTLEになるので、かぶらないようにする
+POW = [pow(4, i) for i in range(13)]
+
+
+def insert(dic, string):
+    dic[get_key(string)] = string
+
+
+def find(dic, string):
+    # i = 0
+    # while dic[(hash1(string) + i*hash2(string)) % M] and dic[(hash1(string) + i*hash2(string)) % M] != string:
+    #     i += 1
+    # return True if dic[(hash1(string) + i*hash2(string)) % M] == string else False
+    # print(dic[get_key(string)], string)
+    return True if dic[get_key(string)] else False
+
+
+def get_num(char):
+    if char == "A":
+        return 1
+    elif char == "C":
+        return 2
+    elif char == "G":
+        return 3
+    else:
+        return 4
+
+
+def get_key(string):
+    num = 0
+    for i in range(len(string)):
+        num += POW[i] * get_num(string[i])
+    return num
+
+
+def main():
+    n = int(input())
+    dic = [None] * POW[12]
+    for i in range(n):
+        order, string = input().split()
+        if order == "insert":
+            insert(dic, string)
+        else:
+            if find(dic, string):
+                print('yes')
+            else:
+                print('no')
+
+
+if __name__ == "__main__":
+    main()
